f08b9ad118e922a25208bbe8e00d280b55380a8c
Laurine Adhiambo <laurinealuoch7@gmail.com>
2019-12-13 09:17:48 +0300

    admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AdminController extends Controller
 {
     public function index(){
+
+        $admins = User::whereRole_id(2)->orderByDesc('created_at')->get();
+        $enumerators = User::whereRole_id(3)->orderByDesc('created_at')->get();
         
-        return view('admin.welcome');
+        return view('admin.welcome', get_defined_vars());
     }
 
     public function tables(){
